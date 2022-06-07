5b9a16118f4a8a9befcb848d7f3f32fb3732fcc1
Alexander <aleksander1990@rambler.ru>
2022-06-07 19:13:36 +0300

    fourth page community ended, adapted

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,20 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
+
 const hamburger = document.querySelector('.hamburger'),
       mobile = document.querySelector('.mobile'),
       closeElem = document.querySelector('.mobile__close'),
